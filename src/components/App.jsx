35fa321238b1c233a428c2864d2c5e085108728e
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-28 14:30:40 +0300

    Working, Lacking style.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,46 @@
 import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Switch, Route, useParams} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import axios from 'axios';
+import Todo from './Todo';
+import TodoBig from './TodoBig';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+        .then(function (response) {
+            setTodos(response.data);
+        })
+    }, []);
+
+    const todoRope = (id) => {
+      const todosCopy = todos.slice();
+      for (let i = 0; i < todosCopy.length; i++) {
+          if (id === todosCopy[i].id) {
+              todosCopy[i].completed = !todosCopy[i].completed;
+              break;
+          }
+      }
+      setTodos(todosCopy);
+  }
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+    <Switch>
+        <Route path={'/:todoId'}>
+            <div className="todo-container">
+                <TodoBig todos={todos} sniuriukas={todoRope}></TodoBig>
+            </div>
+        </Route>
+        <Route path='/'>
+            <div className="todo-container">
+                {todos.map((todo)=>(<Todo key={todo.id} data={todo} sniuriukas={todoRope}></Todo>))}
+            </div>
+        </Route>
+    </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Todo.jsx
@@ -0,0 +1,19 @@
+import User from './User';
+import {Link} from 'react-router-dom';
+
+function Todo(props) {
+
+    return (
+        <div className="todo">
+            <Link to={'/'+props.data.id}><div className='id'>{props.data.id}</div></Link>
+            <User id={props.data.userId}></User>
+            <div className="title">{props.data.title}</div>
+            <div className="status" onClick={()=>props.sniuriukas(props.data.id)} style={
+                {backgroundColor: props.data.completed === false ? 'red' : 'green'}
+                }></div>
+        </div>
+    );
+}
+
+
+export default Todo;
