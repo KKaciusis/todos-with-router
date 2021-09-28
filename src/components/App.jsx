5a15efb6497f4303d2bbbf386377cf030d11843b
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-28 14:19:07 +0300

    axios, react-router installed, imports done.

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Switch, Route, useParams} from 'react-router-dom';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
